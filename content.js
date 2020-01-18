1697bf574750fc71e0c832a971fc5183b39ef3f4
rkpattnaik780 <rkpattnaik780@gmail.com>
2020-01-18 15:31:13 +0530

    Feat: hit API in the front-end
    The front-end now extracts the labels and gets them translated to Hindi.

--- content.js
+++ content.js
@@ -1,9 +1,33 @@
 var labels = document.getElementsByTagName('label');
 var output_labels=[]
 
-for (var i = 0, l = labels.length; i < l; i++) {
+for (let i = 0, l = labels.length; i < l; i++) {
   output_labels[i] = labels[i].innerText;
 }
-console.log(labels)
-console.log(output_labels)
+
+// TODOs : Have the original labels stored such that we dont need to hit
+// APIs while toggling between languages.
+
+$.ajax({
+  url: "https://hackapi.reverieinc.com/nmt",
+  headers: { 
+    'Content-Type':'application/json',
+    'token': '106c1c674bd9b81ece4194667167f68ba46fbe03'
+  },
+  method: 'POST',
+  dataType: 'json',
+  "data": JSON.stringify({
+    "data" : output_labels,
+    "tgt" : "hi",
+    "src" : "en"
+  }),
+  success: function(res){
+    console.log(res.data.result.flat(1));
+    var translatedLabels = res.data.result.flat(1);
+    for (let i = 0, l = labels.length; i < l; i++) {
+      labels[i].innerText = translatedLabels[i];
+    }
+    console.log(labels);
+  }
+});
 
